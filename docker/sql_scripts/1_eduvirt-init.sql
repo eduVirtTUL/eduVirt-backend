eaace5b7972f1b6d18aaea8bb6e033d09f3316e7
242447 Kwiatkowski Piotr <242447@edu.p.lodz.pl>
2024-12-03 19:55:04 +0100

    Develop_pk branch rebase.

--- docker/sql_scripts/1_eduvirt-init.sql
+++ docker/sql_scripts/1_eduvirt-init.sql
@@ -1,11 +1,7 @@
 -- User
 CREATE USER eduvirt PASSWORD 'pass';
 
--- Schema
-CREATE SCHEMA eduvirt;
-
-ALTER SCHEMA eduvirt OWNER TO eduvirt;
-
 -- Grants
 GRANT SELECT, REFERENCES ON public.vnic_profiles TO eduvirt;
 GRANT SELECT ON public.cluster TO eduvirt;
+
