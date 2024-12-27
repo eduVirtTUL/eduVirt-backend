043ce9e0a9d67694f4e495c4ab0481ddc8305fec
Adam Czerwonka <adamczerwonka59@gmail.com>
2024-12-27 14:05:35 +0100

    feat: update private segment model to include vm info

--- src/main/java/pl/lodz/p/it/eduvirt/repository/ResourceGroupNetworkRepository.java
+++ src/main/java/pl/lodz/p/it/eduvirt/repository/ResourceGroupNetworkRepository.java
@@ -1,20 +1,13 @@
 package pl.lodz.p.it.eduvirt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.lodz.p.it.eduvirt.entity.ResourceGroupNetwork;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ResourceGroupNetworkRepository extends JpaRepository<ResourceGroupNetwork, UUID> {
     List<ResourceGroupNetwork> getAllByResourceGroupId(UUID id);
-
-    @Query("SELECT n FROM ResourceGroupNetwork n JOIN n.interfaces i WHERE i = :id")
-    Optional<ResourceGroupNetwork> getNetworkByInterface(UUID id);
-
-    Optional<ResourceGroupNetwork> findByResourceGroupIdAndInterfacesContains(UUID id, UUID nicId);
 }
