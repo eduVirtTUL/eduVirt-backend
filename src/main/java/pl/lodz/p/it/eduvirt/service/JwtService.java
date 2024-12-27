e58007a447c7b58e750ac5b7ade88ed7adf00f4a
Adam Czerwonka <adamczerwonka59@gmail.com>
2024-12-27 15:59:00 +0100

    feat: change auth configuration to use keycloack token only

--- src/main/java/pl/lodz/p/it/eduvirt/service/JwtService.java
+++
@@ -1,9 +0,0 @@
-package pl.lodz.p.it.eduvirt.service;
-
-import pl.lodz.p.it.eduvirt.entity.User;
-
-import java.util.List;
-
-public interface JwtService {
-    String generateToken(User user, List<String> roles);
-}
