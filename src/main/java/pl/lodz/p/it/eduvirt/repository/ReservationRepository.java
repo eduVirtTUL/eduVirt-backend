d7c901f15c0f73c3fd92739769b8b35a4f91ce6e
Michał Piestrzeniewicz <michal.p2608@wp.pl>
2024-12-28 23:37:56 +0100

    feat: Improvments in the executor algorithm implementation

--- src/main/java/pl/lodz/p/it/eduvirt/repository/ReservationRepository.java
+++ src/main/java/pl/lodz/p/it/eduvirt/repository/ReservationRepository.java
@@ -49,4 +49,22 @@ public interface ReservationRepository extends JpaRepository<Reservation, UUID> 
 //    @Query("SELECT r FROM Reservation r WHERE r.endTime > current_timestamp() " +
 //            "AND r.team IN (SELECT t FROM Team t WHERE t.course.clusterId = :cluster)")
 //    List<Reservation> findAllActiveReservationsForCluster(@Param("cluster") UUID clusterId);
+
+    //TODO michal: maybe add flag to reservation or check executor_task table to check which reservation was processing
+    //TODO michal: optimization
+    //TODO michal: change r.endTime to 'r.endTime - 5 minutes' for ex. -> starting reservations for a few seconds makes no sense..
+    @Query("""
+            SELECT DISTINCT r FROM Reservation r
+            WHERE current_timestamp BETWEEN r.startTime AND r.endTime
+            AND r.id NOT IN (SELECT et.reservation.id FROM ExecutorTask et WHERE et.type = 'POD_INIT' AND et.status != 'FAILED')
+            """)
+    List<Reservation> findReservationsToBegin();
+
+    //TODO michal: r.endTime - 5 minutes -> due to the potential start of the next reservation immediately after this one
+    @Query("""
+            SELECT DISTINCT r FROM Reservation r
+            WHERE current_timestamp >= r.endTime
+            AND r.id NOT IN (SELECT et.reservation.id FROM ExecutorTask et WHERE et.type = 'POD_DESTRUCT' AND et.status != 'FAILED')
+            """)
+    List<Reservation> findReservationsToFinish();
 }
