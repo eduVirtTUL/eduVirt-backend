357b859dac1491ddd4aedca0f43a5182a571b6fe
Adam Czerwonka <adamczerwonka59@gmail.com>
2025-01-21 19:00:33 +0100

    feat: minor changes to base entity classes

--- src/main/java/pl/lodz/p/it/eduvirt/entity/Updatable.java
+++ src/main/java/pl/lodz/p/it/eduvirt/entity/Updatable.java
@@ -5,11 +5,14 @@ import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Version;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @MappedSuperclass
 @Getter
 @NoArgsConstructor
-public class Updatable extends AbstractEntity {
+public abstract class Updatable extends AbstractEntity {
 
     @Version
     @Column(name = "version", nullable = false)
@@ -17,7 +20,23 @@ public class Updatable extends AbstractEntity {
 
     /* Constructors */
 
-    public Updatable(Long version) {
+    protected Updatable(Long version) {
         this.version = version;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Updatable updatable = (Updatable) o;
+        return getId() != null && Objects.equals(getId(), updatable.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 }
