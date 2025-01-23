97deb855ffb860244d3e5fae4054b83818ae6d4a
Bartłomiej Kałach <242414@edu.p.lodz.pl>
2025-01-23 17:07:42 +0100

    refactor: updated key generator

--- src/main/java/pl/lodz/p/it/eduvirt/util/key/KeyGenerationConstants.java
+++ src/main/java/pl/lodz/p/it/eduvirt/util/key/KeyGenerationConstants.java
@@ -1,8 +1,8 @@
 package pl.lodz.p.it.eduvirt.util.key;
 
 public class KeyGenerationConstants {
-    public static final String ALLOWED_CHARS = "23456789ABCDEFGHJKLMNPQRSTUVWXYZ";
-    public static final int KEY_LENGTH = 8;
+    public static final String ALLOWED_CHARS = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    public static final int KEY_LENGTH = 12;
     public static final int MAX_ATTEMPTS = 10;
 
     private KeyGenerationConstants() {}
